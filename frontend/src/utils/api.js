399691e00910026fd3229d2a665d93ead5dd4a2c
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-09-29 14:31:52 -0500

    updated .env files to run locally and on production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 
-const BASE_URL=import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
-// --- HELPERS ---
+const BASE_URL=import.meta.env.VITE_API_URL 
 
+// --- API HELPERS ---
 // 1. Register
 export const registerUser = async (username, email, password) => {
   const response = await fetch(`${BASE_URL}/api/register`, {
